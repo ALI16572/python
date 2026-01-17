0546307f3fddf4cb6d21bd2536634253fe3c769b
ALI16572 <raks79107@gmail.com>
2026-01-17 16:26:57 -0600

    Add files via upload

---
+++ average.py
@@ -0,0 +1,12 @@
+#Jack has five trees in his front yard. He measures the height of each tree in inches and records the values: 98, 94, 41, 96, and 11. What is the average height of the trees in Jack’s front yard?
+
+a = 98 
+b = 94
+c = 41
+d = 96
+e = 11
+
+sum = a + b + c +d + e
+print(sum)
+average = sum/5
+print(average)
