070e88d59da4be190d47b102a7ddde39b795f3cf
ALI16572 <raks79107@gmail.com>
2026-01-18 15:11:01 -0600

    Add files via upload

---
+++ rootsqaure.py
@@ -0,0 +1,10 @@
+import math
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Calculate square root
+sqrt_value = math.sqrt(num)
+
+# Display the result
+print("The square root of", num, "is", sqrt_value)
