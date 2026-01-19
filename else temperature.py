71bdc0b5286e5e6b0ca730d7040004cbed2a4e64
ALI16572 <raks79107@gmail.com>
2026-01-19 16:10:08 -0600

    Add files via upload

---
+++ else temperature.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter a temperature in C:"))
+
+if temperature >= 25:
+    print("You can wear light or soft clothes.")
+else:
+    print("You can wear clothes like a jacket or pullover.")
