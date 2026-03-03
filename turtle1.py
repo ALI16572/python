3c70fa1d39d6fd32bad83e949b28a6d0d9b95af1
ALI16572 <raks79107@gmail.com>
2026-03-02 22:35:39 -0600

    Add files via upload

--- turtle1.py
+++ turtle1.py
@@ -1,11 +1,27 @@
+# import turtle 
+# turtle.Screen().bgcolor("orange")
+# turtle.Screen().setup(600,400)
+# polygon=turtle.Turtle()
+# side = 6
+# length = 70
+# angle = 360.0/side
+# for i in range(side):
+#     polygon.forward(length)
+#     polygon.right(angle)
+# turtle.done()    
+
 import turtle
-turtle.Screen().bgcolor("orange")
-turtle.Screen().setup(600,400)
-polygon=turtle.Turtle()
-side = 6
-length = 70
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(500,400)
+square=turtle.Turtle()
+side = 4
+length = 400
 angle = 360.0/side
 for i in range(side):
-    polygon.forward(length)
-    polygon.right(angle)
+    square.forward(length)
+    square.right(angle)
 turtle.done()    
+
+
+
+
