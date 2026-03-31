e5941dc0007ada9047add5280d7df4281bcb4b66
ALI16572 <raks79107@gmail.com>
2026-03-31 15:01:17 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+square_value = int(input("Enter a number: "))
+
+square = square_value ** 2   # this squares the number
+
+if square % 2 == 0:
+    print("Square is even")
+else:
+    print("Square is odd")  
